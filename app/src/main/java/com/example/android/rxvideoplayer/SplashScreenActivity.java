cba69edf8955cb74b227c2cc15817732ea54782c
Rushikesh Kate <rushikeshkate8@gmail.com>
2020-04-30 16:05:36 +0530

    nothing

--- app/src/main/java/com/example/android/rxvideoplayer/SplashScreenActivity.java
+++ app/src/main/java/com/example/android/rxvideoplayer/SplashScreenActivity.java
@@ -1,26 +1,47 @@
 package com.example.android.rxvideoplayer;
 
+
+
 import android.graphics.Color;
+
 import android.os.Bundle;
+
 import android.view.View;
+
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
 import gr.net.maroulis.library.EasySplashScreen;
+
+
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-        EasySplashScreen config = new EasySplashScreen(SplashScreenActivity.this)
-                .withFullScreen()
-                .withTargetActivity(MainActivity.class)
-                .withSplashTimeOut(1000)
-                .withBackgroundColor(Color.parseColor("#FFFFFF"))
-                .withLogo(R.drawable.ic_play_circle_filled_black_24dp);
-        View easySplashScreen = config.create();
-        setContentView(easySplashScreen);
-    }
+
+  @Override
+
+  protected void onCreate(Bundle savedInstanceState) {
+
+    super.onCreate(savedInstanceState);
+
+
+
+    EasySplashScreen config = new EasySplashScreen(SplashScreenActivity.this)
+
+            .withFullScreen()
+
+            .withTargetActivity(MainActivity.class)
+
+            .withSplashTimeOut(1000)
+
+            .withBackgroundColor(Color.parseColor("#FFFFFF"))
+
+            .withLogo(R.drawable.ic_play_circle_filled_black_24dp);
+    View easySplashScreen = config.create();
+    setContentView(easySplashScreen);
+  }
 }
